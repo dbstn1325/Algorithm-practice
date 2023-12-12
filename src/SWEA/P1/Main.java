34c5cf5f9ad71011719c4ca5607ba95603386aa9
dbstn1325 <dbstn1325@naver.com>
2023-12-12 23:46:52 +0900

    [swea-p1]

---
+++ src/SWEA/P1/Main.java
@@ -0,0 +1,56 @@
+package SWEA.P1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+    public static long ans = 0;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/SWEA/P1/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        long[] p = new long[N];
+        String[] input = br.readLine().split(" ");
+        for(int i=0; i<N; i++){
+            p[i] = Integer.parseInt(input[i]);
+        }
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(stk.nextToken());
+        int l = Integer.parseInt(stk.nextToken());
+
+        for(int i=0; i<N; i++){
+            ans++;
+            p[i] -= m;
+        }
+
+        for(int i=0; i<N; i++){
+            if(p[i] <= 0) {
+                continue;
+            }
+
+            if(p[i] <= l) {
+                ans++;
+            }
+
+            if(p[i] > l) {
+                long r = 0;
+                r += p[i] / l;
+                if(p[i] % l > 0) {
+                    r++;
+                };
+
+                ans += r;
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
